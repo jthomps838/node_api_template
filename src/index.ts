ba9b4aad4fd5f36cb26c643d5323c4d77adf82b7
Joseph Thompson <joe.thompson838@gmail.com>
2024-07-13 18:24:51 -0400

    feat: add swagger docs examples

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,23 @@ import express, { Application, Router } from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc, {Options, SwaggerDefinition} from 'swagger-jsdoc';
 
 import userRoutes from './routes/userRoutes';
 import prisma from './prisma';
 
 const PORT = process.env.PORT || 8000;
 const app: Application = express();
-const router: Router = express.Router();
+
+// Swagger setup
+const swaggerOptions: Options = {
+    swaggerDefinition: require('./swagger.json'),
+    apis: ['./routes/*.js'], // Path to your route files
+  };
+
+  const swaggerDocs = swaggerJsdoc(swaggerOptions);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Middleware
 app.use(express.json());
@@ -19,6 +29,25 @@ app.use(morgan('dev'));
 //  Routes
 app.use('/api/users', userRoutes);
 
+
+/**
+ * @swagger
+ * /:
+ *   get:
+ *     summary: API Health Check
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: documentation about current API
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               items:
+ *                 $ref: '#/components/schemas/Health'
+ *       500:
+ *         description: Internal server error
+ */
 app.get('/', (req, res) => {
     res.status(200).json({
         version: '0.0.1',
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,34 @@
-import { Router } from 'express';
+import express, { Router, Request, Response} from 'express';
 import prisma from '../prisma';
 
-const router = Router();
+const router: Router = express.Router();
 
-router.get('/', async (req, res) => {
+/**
+ * @swagger
+ * tags:
+ *   name: Users
+ *   description: User management
+ */
+
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ */
+router.get('/', async (req: Request, res: Response) => {
   try {
     const users = await prisma.user.findMany();
     res.json(users);
@@ -12,7 +37,39 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.post('/', async (req, res) => {
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - email
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 description: The name of the user
+ *               email:
+ *                 type: string
+ *                 description: The email of the user
+ *     responses:
+ *       201:
+ *         description: User created
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: Bad request
+ */
+router.post('/', async (req: Request, res: Response) => {
   try {
     const { name, email } = req.body;
     const user = await prisma.user.create({
